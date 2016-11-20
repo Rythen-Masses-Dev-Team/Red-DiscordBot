53b87d327885fe773a059f73d7c2b6b06398614d
thegreatestdjhero <loverockband@gmail.com>
2016-11-20 20:36:10 +0000

    Added Start Bash Script With While Loop

---
+++ start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+clear
+echo "================================================"
+echo "-------------------RedBot-----------------------"
+echo "================================================"
+sleep 5
+while true; do 
+clear
+echo "Starting Redbot"
+sleep 3
+python3.5 red.py
+echo "Please Wait (10 Sec)"
+sleep 10
+echo "Qutting"
+sleep 1
+echo "press Ctl^C to Exit (5 Sec)"
+sleep 10
+done
+exit 0
